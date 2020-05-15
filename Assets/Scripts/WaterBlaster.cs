0f1b2c6cef236fab020c6b88aff9ef1be5fd14ea
amman98 <45375247+amman98@users.noreply.github.com>
2020-05-15 11:59:32 -0700

    Add files via upload

---
+++ Assets/Scripts/WaterBlaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBlaster : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private float speed = 10f;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f;
+        rb.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
