1978e1936ec7bb0924c390b4064a4c824ce60436
amman98 <45375247+amman98@users.noreply.github.com>
2020-05-30 22:52:40 -0700

    Minimap added to Level 3
    Also fixed collision issues with water blaster in the second level.

---
+++ Assets/Scripts/UIScript/MinimapFollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.z = transform.position.z;
+        transform.position = newPos;
+    }
+}
