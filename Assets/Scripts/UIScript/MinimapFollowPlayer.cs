ea27bc46fcbdcb91eaec6cd2baa7be31afcc713e
Gabriel Oliver <gabriel-a-oliver@users.noreply.github.com>
2020-06-01 23:07:49 -0700

    update to minimap

--- Assets/Scripts/UIScript/MinimapFollowPlayer.cs
+++ Assets/Scripts/UIScript/MinimapFollowPlayer.cs
@@ -6,6 +6,11 @@ public class MinimapFollowPlayer : MonoBehaviour
 {
     public Transform player;
 
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
     void LateUpdate()
     {
         Vector3 newPos = player.position;
