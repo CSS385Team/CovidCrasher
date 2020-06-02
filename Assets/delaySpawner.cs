ce8db334ab56d70a28509603dc45406cabf598fa
root <root@DESKTOP-02291TS.localdomain>
2020-06-02 01:14:17 -0700

    Minimap added to all map + delay Spawner added

---
+++ Assets/delaySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delaySpawner : MonoBehaviour
+{
+    public float delay = 6;
+    public GameObject spawner;
+
+    void Start()
+    {
+        delay = 6f;
+        Invoke("enableSpawner", delay);
+    }
+
+    public void enableSpawner()
+    {
+        spawner.SetActive(true);
+    }
+}
