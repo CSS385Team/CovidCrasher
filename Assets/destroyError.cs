ce43c2aeb4a08ff1a309aa92c29e5535d5f22b1a
root <root@DESKTOP-02291TS.localdomain>
2020-06-09 13:30:48 -0700

    main menu destroy stuffs (hopefully it works)

--- Assets/destroyError.cs
+++ Assets/destroyError.cs
@@ -13,6 +13,28 @@ public class destroyError : MonoBehaviour
             Destroy(GameObject.FindGameObjectWithTag("UI"));
         if (GameObject.FindGameObjectWithTag("Music") != null)
             Destroy(GameObject.FindGameObjectWithTag("Music"));
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        if (GameObject.FindGameObjectWithTag("UI") != null)
+            Destroy(GameObject.FindGameObjectWithTag("UI"));
+        if (GameObject.FindGameObjectWithTag("Music") != null)
+            Destroy(GameObject.FindGameObjectWithTag("Music"));
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        if (GameObject.FindGameObjectWithTag("UI") != null)
+            Destroy(GameObject.FindGameObjectWithTag("UI"));
+        if (GameObject.FindGameObjectWithTag("Music") != null)
+            Destroy(GameObject.FindGameObjectWithTag("Music"));
+
+
+        if (GameObject.Find("DoctorPlayer(Clone)") != null)
+            Destroy(GameObject.Find("DoctorPlayer(Clone)"));
+        if (GameObject.Find("UI(Clone)") != null)
+            Destroy(GameObject.Find("UI(Clone)"));
+        if (GameObject.Find("LevelMusic(Clone)") != null)
+            Destroy(GameObject.Find("LevelMusic(Clone)"));
     }
     
     
